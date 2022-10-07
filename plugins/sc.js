97e808d11a6c5f2fcf2b8b791631ebed017755c7
faruq51 <jdhshuwwhwu@gmail.com>
2022-10-07 19:27:24 +0700

    Loss doll

---
+++ plugins/sc.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn, usedPrefix }) => { 
+      conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `
+╭─「 Mau Esceh??? 」
+│ • Sc ini Private bang
+│ • Pc gw wa.me/6285807264974
+╰────
+`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
+}
+
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = /^sc$/i
+
+module.exports = handler
